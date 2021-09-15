cee78713ca73560e337db38b43833856bbcd3c33
alecdwm <alec@owls.io>
2021-09-15 18:15:33 +1000

    added isPending isResolved isRejected to Deferred

--- src/deferred.ts
+++ src/deferred.ts
@@ -1,15 +1,37 @@
 export function Deferred<T>(): {
+  promise: Promise<T>
   resolve: (value: T | PromiseLike<T>) => void
   reject: (reason?: any) => void
-  promise: Promise<T>
+  isPending: () => boolean
+  isResolved: () => boolean
+  isRejected: () => boolean
 } {
   let resolve!: (value: T | PromiseLike<T>) => void
   let reject!: (reason?: any) => void
 
-  const promise = new Promise<T>((_resolve, _reject) => {
-    resolve = _resolve
-    reject = _reject
+  let isPending = true
+  let isResolved = false
+  let isRejected = false
+
+  const promise = new Promise<T>((innerResolve, innerReject) => {
+    resolve = value => {
+      isPending = false
+      isResolved = true
+      innerResolve(value)
+    }
+    reject = reason => {
+      isPending = false
+      isRejected = true
+      innerReject(reason)
+    }
   })
 
-  return { resolve, reject, promise }
+  return {
+    promise,
+    resolve,
+    reject,
+    isPending: () => isPending,
+    isResolved: () => isResolved,
+    isRejected: () => isRejected,
+  }
 }
